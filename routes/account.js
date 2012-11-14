02642c069b3ad53d578702f705524cd66fc3f86d
trong <vuminhtrong>
2012-11-14 13:34:25 +0700

    move database to mongolab

--- routes/account.js
+++ routes/account.js
@@ -12,6 +12,7 @@ var express = require("express");
 var app = express();
 //var mongo;
 
+/*
 app.configure('development', function(){
     mongo = {
         "hostname":"localhost",
@@ -22,17 +23,44 @@ app.configure('development', function(){
         "db":"taxi"
     }
 });
+
+// default database of appfog
 app.configure('production', function(){
     var env = JSON.parse(process.env.VCAP_SERVICES);
     mongo = env['mongodb-1.8'][0]['credentials'];
+});
+*/
+//mongodb://<dbuser>:<dbpassword>@ds037827.mongolab.com:37827/taxivn
+//https://mongolab.com/databases/taxivn#users
+app.configure('production', function(){
+    mongo = {
+        "hostname":"ds037827.mongolab.com",
+        "port":37827,
+        "username":"trong",
+        "password":"123456",
+        "name":"",
+        "db":"taxivn"
+    }
+});
+app.configure('development', function(){
+    mongo = {
+        "hostname":"ds037827.mongolab.com",
+        "port":37827,
+        "username":"trong",
+        "password":"123456",
+        "name":"",
+        "db":"taxivn"
+    }
 });
 var generate_mongo_url = function(obj){
     obj.hostname = (obj.hostname || 'localhost');
     obj.port = (obj.port || 27017);
     obj.db = (obj.db || 'test');
     if(obj.username && obj.password){
+    	console.log("mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db);
         return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
     }else{
+    	console.log("mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db);
         return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
     }
 }
